0d05ca38272738b19cf7a6dc0c948d2e8e190ccc
Alcuavi <alberto.cuvi@gmail.com>
2024-04-19 18:27:31 +0200

    Creating the HTTP server with express

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { API_VERSION } = require("./constants");
+
+const app = express();
+
+// Import routings
+// ...
+
+// Configure Body Parse
+// ...
+
+//Configure Static Folder
+// ...
+
+// Configure Header HTTP - CORS
+// ...
+
+// Configure routings
+// ...
+
+module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const app = require("./app");
 
 const {
     DB_USER, 
@@ -8,10 +9,19 @@ const {
     IP_SERVER
 } = require("./constants");
 
+const PORT = process.env.POST || 3977;
+
 const connectDB = async () => {
     try {
       await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/`)
         console.log('La conexión con la base de datos ha sido exitosa.');  
+
+        app.listen(PORT, () => {
+          console.log("##################");
+          console.log("#### API REST ####");
+          console.log("##################");
+          console.log(`http://${IP_SERVER}:${PORT}/api/${API_VERSION}`);    
+          });
         
     } catch (err) {
         console.log('Error al conectar a la base de datos', err);
