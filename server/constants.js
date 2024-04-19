cc87231c4f6f465bb8adc6bf4b0b12fdcd6d3075
Alcuavi <alberto.cuvi@gmail.com>
2024-04-19 18:13:55 +0200

    Connecting the application with the database - Mongoose

---
+++ server/constants.js
@@ -0,0 +1,14 @@
+const DB_USER = "admin";
+const DB_PASSWORD = "admin1234";
+const DB_HOST = "web-personal.qlltefj.mongodb.net";
+
+const API_VERSION = "v1";
+const IP_SERVER = "localhost";
+
+module.exports = {
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    API_VERSION,
+    IP_SERVER
+}
