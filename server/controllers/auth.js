5074200b32e7c6d25decc1d6755b0fc626542386
Alcuavi <alberto.cuvi@gmail.com>
2024-04-19 22:29:02 +0200

    User register

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,31 @@
-async function register(req, res) { 
-    console.log("Se ha ejecutado el registro");
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
 
-    res.status(200).send({msg: "Todo OK"});
+async function register(req, res) { 
+    const {firstname, lastname, email, password} = req.body;
+
+    if(!email) res.status(400).send({msg: "El email es obligatorio"});
+    if(!password) res.status(400).send({msg: "La contraseña es obligatoria"});
+
+    const user = new User({
+        firstname,
+        lastname,
+        email: email.toLowerCase(),
+        role: "user",
+        active: false
+    });
+
+    const salt = bcrypt.genSaltSync(10);
+    const hashPassword = bcrypt.hashSync(password, salt);
+    user.password = hashPassword;
+
+    try {
+        await user.save(); 
+        res.status(200).send({msg: "Usuario guardado"});
+    } catch (err) {
+        res.status(400).send({ msg: `Error al crear el usuario: ${err}`});
+    }
+
 }
 
 module.exports = {
