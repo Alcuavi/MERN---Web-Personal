84662966db75107e7dce8708a7f7f04e37eb0dda
Alcuavi <alberto.cuvi@gmail.com>
2024-04-20 21:11:01 +0200

    Get the data of the logged in user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,16 @@
+const User = require("../models/user");
+
 async function getMe(req, res) {
-    res.status(200).send({msg: "OK" });
+
+    const {user_id} = req.user;
+
+    const response = await User.findById(user_id);
+
+    if (!response) {
+        res.status(400).send({msg: "No se ha encontrado usuario"});
+    } else {
+        res.status(200).send(response);
+    }    
 }
 
 module.exports = {
