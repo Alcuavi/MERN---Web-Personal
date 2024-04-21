01c803d8d6dfccfce13ddb59f1daaeec3c11858a
Alcuavi <alberto.cuvi@gmail.com>
2024-04-21 19:26:33 +0200

    Creation users

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 
 async function getMe(req, res) {
@@ -25,11 +26,33 @@ async function getUsers(req, res) {
     }
 
     res.status(200).send(response);
+}
 
+async function createUser(req, res) {
+    
+    const {password} = req.body;
+    const user = new User({...req.body, active: false});
 
+    const salt = bcrypt.genSaltSync(10);
+    const hasPassword = bcrypt.hashSync(password, salt);
+    user.password = hasPassword;
+
+    if (req.files.avatar) {
+        //TODO:
+        console.log("Procesar avatar");
+    }
+
+    try {
+        const response = await user.save();
+        res.status(200).send(response);
+    } catch (err) {
+        res.status(400).send({msg: `Error al crear el usuario: ${err}`});
+    }
 }
 
 module.exports = {
     getMe,
-    getUsers
+    getUsers,
+    createUser
 };
+
