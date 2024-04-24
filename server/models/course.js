b9dee8b7d1a2534b7d59584119d48caea4b7ee48
Alcuavi <alberto.cuvi@gmail.com>
2024-04-24 13:18:33 +0200

    Course model

---
+++ server/models/course.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const CourseSchema = mongoose.Schema({
+    title: String,
+    miniature: String,
+    description: String,
+    url: String,
+    price: Number,
+    score: Number
+});
+
+module.exports = mongoose.model("Course", CourseSchema);
