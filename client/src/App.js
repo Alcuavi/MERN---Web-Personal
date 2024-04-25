037381e9a5bf3c74b6d1ae20d7f2fc0525bed67c
Alcuavi <alberto.cuvi@gmail.com>
2024-04-25 12:37:08 +0200

    Installing Semantic UI React

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { Button } from 'semantic-ui-react'
 
 export default function App() {
   return (
     <div>
       <h1>Estamos en mi web</h1>
+      <div>
+        <Button primary>Primary</Button>
+        <Button secondary>Secondary</Button>
+      </div>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
