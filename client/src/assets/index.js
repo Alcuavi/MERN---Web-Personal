436f868368321551bd8a55b9e6645762e09b93ae
Alcuavi <alberto.cuvi@gmail.com>
2024-04-25 12:58:35 +0200

    Assets

---
+++ client/src/assets/index.js
@@ -0,0 +1,23 @@
+import {ReactComponent as LogoWhite} from "./svg/tincode-white.svg";
+
+import authBg from "./jpg/auth-bg.jpg";
+import HomeBanner from "./jpg/home-banner.jpg";
+import noAvatar from "./jpg/no-avatar.jpg";
+import academyLogo from "./png/academy-logo.png";
+
+
+const Icon = {
+    LogoWhite
+};
+
+const image = {
+    authBg,
+    HomeBanner,
+    noAvatar,
+    academyLogo
+};
+
+export { 
+    Icon,
+    image
+ };
