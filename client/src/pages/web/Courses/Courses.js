fc7ccab4a0d45ac53d629665378d945ee83c8063
Alcuavi <alberto.cuvi@gmail.com>
2024-04-29 17:48:06 +0200

    Creating the web pages

---
+++ client/src/pages/web/Courses/Courses.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Courses() {
+  return (
+    <div>
+      <h1>Estamos en la pagina de cursos</h1>
+    </div>
+  );
+}
---
+++ client/src/pages/web/Post/Post.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Post() {
+  return (
+    <div>
+      <h1>Estamos en la pagina del Post</h1>
+    </div>
+  );
+}
