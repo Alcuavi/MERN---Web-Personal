e4ee7faa475f7ef5354e98c5a10c695ff838840f
Alcuavi <alberto.cuvi@gmail.com>
2024-05-07 10:10:05 +0200

    Validating form with Yup

--- client/src/components/Admin/Auth/RegisterForm/RegisterForm.form.js
+++ client/src/components/Admin/Auth/RegisterForm/RegisterForm.form.js
@@ -1,3 +1,5 @@
+import * as Yup from "yup";
+
 export function initialValues() {
     return {
         email: "",
@@ -5,4 +7,13 @@ export function initialValues() {
         repeatPassword: "",
         conditionsAccepted: false,
     };
+}
+
+export function validationSchema () {
+    return Yup.object({
+        email: Yup.string().email("El email no es valido").required("Campo obligatorio"),
+        password: Yup.string().required("Campo obligatorio"),
+        repeatPassword: Yup.string().required("Campo obligatorio").oneOf([Yup.ref("password")], "Las contraseñas tienen que ser iguales"),
+        conditionsAccepted: Yup.bool().isTrue(true),
+    })
 }
--- client/src/components/Admin/Auth/RegisterForm/RegisterForm.js
+++ client/src/components/Admin/Auth/RegisterForm/RegisterForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Form } from "semantic-ui-react";
 import { useFormik } from "formik";
-import { initialValues } from "./RegisterForm.form";
+import { initialValues, validationSchema } from "./RegisterForm.form";
 import "./RegisterForm.scss";
 
 export function RegisterForm() {
@@ -9,21 +9,52 @@ export function RegisterForm() {
 
     const formik = useFormik({
         initialValues: initialValues(),
+        validationSchema: validationSchema(),
+        validateOnChange: false,
         onSubmit: async (formValue) => {
             try {
+                setError("");
                 console.log(formValue);
             } catch (error) {
-                console.error(error);
+                setError("Error en el servidor");
             }
         },
     });
 
   return (
     <Form className="register-form" onSubmit={formik.handleSubmit}>
-      <Form.Input name="email" placeholder="Correo electronico" onChange={formik.handleChange} value={formik.values.email}/>
-      <Form.Input name="password" type="password" placeholder="Contraseña" onChange={formik.handleChange} value={formik.values.password}/>
-      <Form.Input name="repeatPassword" type="password" placeholder="Repetir contraseña" onChange={formik.handleChange} value={formik.values.repeatPassword}/>
-      <Form.Checkbox name="conditionsAccepted" label="He leido y acepto las politicas de privacidad" onChange={(_, data) => formik.setFieldValue("conditionsAccepted", data.checked)} checked={formik.values.conditionsAccepted}/>
+      <Form.Input 
+        name="email" 
+        placeholder="Correo electronico" 
+        onChange={formik.handleChange} 
+        value={formik.values.email}
+        error={formik.errors.email}
+        />
+      <Form.Input 
+        name="password" 
+        type="password" 
+        placeholder="Contraseña" 
+        onChange={formik.handleChange} 
+        value={formik.values.password}
+        error={formik.errors.password}
+        />
+      <Form.Input 
+        name="repeatPassword" 
+        type="password" 
+        placeholder="Repetir contraseña" 
+        onChange={formik.handleChange} 
+        value={formik.values.repeatPassword}
+        error={formik.errors.repeatPassword}
+        />
+      <Form.Checkbox 
+        name="conditionsAccepted" 
+        label="He leido y acepto las politicas de privacidad" 
+        onChange={(_, data) => 
+            formik.setFieldValue("conditionsAccepted", data.checked)
+        } 
+        checked={formik.values.conditionsAccepted}
+        error={formik.errors.conditionsAccepted}
+        />
       
       <Form.Button type="submit" primary fluid loading={formik.isSubmitting}>
         Crear cuenta
