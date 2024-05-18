f09640ea2fa79ebb1837ec0b3a5568f7984453f6
Alcuavi <alberto.cuvi@gmail.com>
2024-05-18 18:32:36 +0200

    User page structure

--- client/src/pages/admin/Users/Users.js
+++ client/src/pages/admin/Users/Users.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Tab, Button } from "semantic-ui-react";
+import "./Users.scss";
 
 export function Users() {
+
+  const panes = [
+    {
+      menuItem: "Usuarios activos",
+      render: () => (
+        <Tab.Pane attached={false}>
+          <h2>Usuarios activos</h2>
+        </Tab.Pane>
+      )
+    },
+    {
+      menuItem: "Usuarios inactivos",
+      render: () => (
+        <Tab.Pane attached={false}>
+          <h2>Usuarios inactivos</h2>
+        </Tab.Pane>
+      )
+    },
+  ]
   return (
-    <div>
-      <h1>Estamos en Users</h1>
-    </div>
+    <>
+      <div className="users-page">
+        <Button className="users-page__add" primary onClick={() => console.log("Abrir formulario")}>
+          Nuevo usuario
+        </Button>
+        <Tab menu={{secondary: true}} panes={panes} />
+      </div>
+    </>
   );
 }
