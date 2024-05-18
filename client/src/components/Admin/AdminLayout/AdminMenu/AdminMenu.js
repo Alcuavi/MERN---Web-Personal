9a3d95ac1b635f3350d8ac760f2392c672305fa4
Alcuavi <alberto.cuvi@gmail.com>
2024-05-18 17:57:25 +0200

    Activating active menu

--- client/src/components/Admin/AdminLayout/AdminMenu/AdminMenu.js
+++ client/src/components/Admin/AdminLayout/AdminMenu/AdminMenu.js
@@ -1,32 +1,39 @@
 import React from 'react';
 import { Menu, Icon } from "semantic-ui-react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./AdminMenu.scss";
 
 export function AdminMenu() {
+    const {pathname} = useLocation();
+    
+    const isCurrentPath = (path) => {
+        if (path === pathname) return true;
+        return false;
+;    }
+
   return (
     <Menu fluid vertical icon text className="admin-menu">
-        <Menu.Item as={Link} to="/admin/users">
+        <Menu.Item as={Link} to="/admin/users" active={isCurrentPath("/admin/users")}>
             <Icon name="user outline"/>
             Usuario
         </Menu.Item>
 
-        <Menu.Item as={Link} to="/admin/menu">
+        <Menu.Item as={Link} to="/admin/menu" active={isCurrentPath("/admin/menu")}>
             <Icon name="bars"/>
             Menu
         </Menu.Item>
 
-        <Menu.Item as={Link} to="/admin/courses">
+        <Menu.Item as={Link} to="/admin/courses" active={isCurrentPath("/admin/courses")}>
             <Icon name="computer"/>
             Cursos
         </Menu.Item>
 
-        <Menu.Item as={Link} to="/admin/newsletter">
+        <Menu.Item as={Link} to="/admin/newsletter" active={isCurrentPath("/admin/newsletter")}>
             <Icon name="mail"/>
             Newsletter
         </Menu.Item>
 
-        <Menu.Item as={Link} to="/admin/blog">
+        <Menu.Item as={Link} to="/admin/blog" active={isCurrentPath("/admin/blog")}>
             <Icon name="comment alternate outline"/>
             Blog
         </Menu.Item>
